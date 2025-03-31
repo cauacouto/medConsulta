a13d73c0a9ac574466064043f1f58a0acb08a1d0
caua <cauacouto022@gmail.com>
2025-03-31 16:32:27 -0300

    mudanças no codigo

---
+++ medCosulta/src/main/java/br/devcouto/com/medCosulta/infra/exception/SecurityFilter.java
@@ -0,0 +1,61 @@
+package br.devcouto.com.medCosulta.infra.exception;
+
+import br.devcouto.com.medCosulta.infra.security.TokenService;
+import br.devcouto.com.medCosulta.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken ( request );
+
+        if (tokenJWT != null) {
+            try {
+                var subject = tokenService.getSubject ( tokenJWT );
+                var usuario = repository.findByLogin ( subject );
+
+                if (usuario != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken( usuario, null, usuario.getAuthorities ( ) );
+                    SecurityContextHolder.getContext ( ).setAuthentication ( authentication );
+                } else{
+                    System.out.println ( "Usuário não encontrado: " + subject );
+                }
+            } catch (RuntimeException e) {
+                System.out.println ( "Erro na validação do token: " + e.getMessage ( ) );
+            }
+        } else{
+            System.out.println ( "Token JWT não encontrado no cabeçalho da requisição." );
+        }
+
+        filterChain.doFilter ( request, response );
+    }
+
+    private String recuperarToken (HttpServletRequest request) {
+        var authorizationHeader = request.getHeader ( "Authorization" );
+        if (authorizationHeader != null && authorizationHeader.startsWith ( "Bearer " )) {
+            String token = authorizationHeader.replace ( "Bearer ", "" );
+            System.out.println ( "Token recuperado: " + token );
+            return token;
+        }
+        System.out.println ( "Token JWT não encontrado no cabeçalho da requisição." );
+        return null;
+    }
+}
--- medCosulta/src/main/java/br/devcouto/com/medCosulta/infra/security/TokenService.java
+++ medCosulta/src/main/java/br/devcouto/com/medCosulta/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.devcouto.com.medCosulta.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,17 +20,32 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario) {
         try {
-            var algoritimo = Algorithm.HMAC256(secret);
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API MedConsulta")
                     .withSubject(usuario.getLogin())
-                    .sign(algoritimo);
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException("Erro ao gerar token");
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API MedConsulta")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
 }
